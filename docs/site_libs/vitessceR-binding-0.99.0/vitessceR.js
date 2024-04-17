a82874c61d1b69cc72bee830ad4b8504c7d31da9
zhenghu <1308155474@qq.com>
2024-04-17 09:12:52 +0800

    add

--- docs/site_libs/vitessceR-binding-0.99.0/vitessceR.js
+++ docs/site_libs/vitessceR-binding-0.99.0/vitessceR.js
@@ -1,3 +1,10 @@
+function asEsModule(component) {
+  return {
+    __esModule: true,
+    default: component,
+  };
+}
+
 HTMLWidgets.widget({
   name: 'vitessceR',
   type: 'output',
@@ -5,8 +12,56 @@ HTMLWidgets.widget({
     // TODO: define shared variables for this instance
 
     return {
-      renderValue: function(x) {
-        render_vitessce(el, x.config, width, height, x.theme);
+      renderValue: async function(x) {
+        const { importWithMap } = await import('https://unpkg.com/dynamic-importmap@0.1.0');
+        const importMap = {
+          imports: {
+            "react": "https://esm.sh/react@18.2.0?dev",
+            "react-dom": "https://esm.sh/react-dom@18.2.0?dev",
+            "react-dom/client": "https://esm.sh/react-dom@18.2.0/client?dev",
+          },
+        };
+        
+        const React = await importWithMap("react", importMap);
+        const { createRoot } = await importWithMap("react-dom/client", importMap);
+        
+        const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+        const e = React.createElement;
+        
+        const jsDevMode = x.js_dev_mode;
+        const customJsUrl = x.custom_js_url;
+        const jsPackageVersion = (x.js_package_version ? x.js_package_version : "3.2.1");
+        const pkgName = (jsDevMode ? "@vitessce/dev" : "vitessce");
+
+        importMap.imports["vitessce"] = (customJsUrl && customJsUrl.length > 0
+            ? customJsUrl
+            : `https://unpkg.com/${pkgName}@${jsPackageVersion}`
+        );
+
+        const { Vitessce } = await importWithMap("vitessce", importMap);
+        
+        function VitessceWidget(props) {
+            const { config, theme } = props;
+            
+            const divRef = React.useRef();
+            
+            const onConfigChange = React.useCallback((newConfig) => {
+                if(window && window.Shiny && window.Shiny.setInputValue) {
+                  Shiny.setInputValue("vitessce_on_config_change", newConfig);
+                }
+            }, [window.Shiny]);
+            
+            const vitessceProps = { height, theme, config, onConfigChange };
+            return e('div', { ref: divRef, style: { height: height + 'px' } },
+              e(React.Suspense, { fallback: e('div', {}, 'Loading...') },
+                e(Vitessce, vitessceProps)
+              )
+            );
+        }
+
+        const root = createRoot(el);
+        root.render(e(VitessceWidget, x));
       },
       resize: function(width, height) {
         // TODO: code to re-render the widget with a new size
